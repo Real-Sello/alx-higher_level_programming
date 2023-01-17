138dc836b967622488a4335e1f05023236c4395b
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-01-17 16:38:00 +0200

    12. Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom = 0
+    for j in range(len(roman_string)):
+        if j > 0 and num[roman_string[j]] > num[roman_string[j - 1]]:
+            rom += num[roman_string[j]] - 2 * \
+                        num[roman_string[j - 1]]
+        else:
+            rom += num[roman_string[j]]
+    return rom
