111646fa666d4be1312180ebbefeb2e1d47e5609
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-01-22 18:53:22 +0200

    Include libraries in header file

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -9,10 +13,11 @@
  * Description: singly linked list node structure
  * for project
  */
+
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
