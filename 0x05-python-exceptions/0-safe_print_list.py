6cece2dfea6f69324d5b20fe0158fd0eab6625f5
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-01-23 13:10:27 +0200

    pycodestyle indentation fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
-             print(i, end="")
-             print("")
+            print(i, end="")
+            print("")
     except IndexError:
         print("")
         return i
