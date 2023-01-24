bd160ce20fd45210d027b42a9f1ae0b01cb8cbd1
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-01-24 22:07:35 +0200

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+
+
+class Square:
+    """a class Square that defines a square"""
+    __size = None
+
+    def __init__(self, size):
+        """Instation with size"""
+        self.__size = size
