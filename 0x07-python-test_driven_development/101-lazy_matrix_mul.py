6c653c8477da8f1175ba384fe52dc195dd2edc6e
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-03 20:59:01 +0200

    7. Lazy matrix multiplication

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+class LazyMatrixMultiplicationError(Exception):
+    pass
+
+def lazy_matrix_mul(m_a, m_b):
+    try:
+        return np.dot(m_a, m_b)
+    except ValueError as e:
+        raise LazyMatrixMultiplicationError("Matrices are not multiplicable") from e
+
+
