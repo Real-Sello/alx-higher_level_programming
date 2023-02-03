4365a25cb4323d23c0d0bbb3d6b41bd61355236b
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-04 00:42:03 +0200

    add max at end test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -29,5 +29,10 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
         self.assertEqual(max_integer([-4, -3, -2, -1]), -1)
 
+    def test_max_at_begginning(self):
+        """Test a list with a beginning max value."""
+        max_at_beginning = [4, 3, 2, 1]
+        self.assertEqual(max_integer(max_at_beginning), 4)
+
 if __name__ == '__main__':
     unittest.main()
