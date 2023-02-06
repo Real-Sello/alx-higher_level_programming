ccec5416238ecc70f9c170c21b8d9a220875aa8a
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-06 19:44:23 +0200

    4. Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function that returns True if the object is an instance of
+    a class that inherited (directly or indirectly)
+    from the specified class ; otherwise False
+"""
+
+def inherits_from(obj, a_class):
+    """Definnes a function that inherits"""
+
+    if type(obj) is a_class or not isinstance(obj, a_class):
+        return False
+    else:
+        return isinstance(obj, a_class)
