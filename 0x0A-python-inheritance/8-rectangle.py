e5dd73db66e18b4da6ad87757f5b84630c1c7df9
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-06 19:59:07 +0200

    8. Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class Rectangle that inherits from BaseGeometry"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defines a class Rectangle"""
+
+    def __init__(self, width, height):
+        """Instantiation with width and height:
+        def __init__(self, width, height):
+            width and height must be private.
+            No getter or setter
+            width and height must be positive integers,
+            validated by integer_validator
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
