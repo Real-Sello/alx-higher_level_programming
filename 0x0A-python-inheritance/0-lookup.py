cf717e90dee4f7c88ba450583f1cdb32b5d878c6
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-06 15:42:46 +0200

    0. Lookup - Pycodestyle Fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Function that return the list of available attributes and methods of an object"""
+"""Function that return the list of available attributes
+    and methods of an object"""
 
 
 def lookup(obj):
