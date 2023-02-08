34cf456fbbc3dec54a3c5f1b734ec09d663b49ba
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-08 23:33:23 +0200

    12. Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Function that returns a list of lists of integers
+representing the Pascal’s triangle of n"""
+
+
+def pascal_triangle(n):
+    """Returns an empty list if n <= 0"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+        else:
+            row = [1]
+            for j in range(i-1):
+                row.append(triangle[i-1][j] + triangle[i-1][j+1])
+            row.append(1)
+            triangle.append(row)
+    return triangle
