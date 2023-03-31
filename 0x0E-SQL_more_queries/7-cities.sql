ae3fe8e7d5e01e2fac3c12636927d78942b020f9
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-03-31 17:47:55 +0200

    7. Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,5 @@
+-- creates database
+-- with tables
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL, state_id INT NOT NULL REFERENCES states(id),	name VARCHAR(256) NOT NULL;
