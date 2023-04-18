34d2d2ace30b5d97618e4c05bbbc27e9612516eb
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-04-18 17:56:10 +0200

    6. First state model|Pycodestyle fixes

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 # Create a declarative base instance
 Base = declarative_base()
 
+
 # Define the State class, which inherits from Base
 class State(Base):
     """
@@ -20,4 +21,3 @@ class State(Base):
     # Define the id and name columns
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
