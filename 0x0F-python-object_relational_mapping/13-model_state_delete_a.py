2efb8d5c04c9a49562331898b8bfa31acbe9ee20
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-04-19 09:23:27 +0200

    13. Delete states|Refactor

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -3,34 +3,35 @@
 script that deletes all State objects with a name containing the
 letter a from the database hbtn_0e_6_usa
 """
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-# Det command line arguments
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+if __name__ == "__main__":
+    """Deletes states from the database"""
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-# Create engine to connect to MySQL server running on localhost
-# at port 3306
-engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                       .format(username, password, database))
+    # Create engine to connect to MySQL server running on localhost
+    # at port 3306
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(username, password, database))
 
-# Using engine to create a session
-Session = sessionmaker(bind=engine)
-session = Session()
+    # Using engine to create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# Query the database to find all State objects with a name
-# containing the letter a
-states_with_a = session.query(State).filter(State.name.like('%a%')).all()
+    # Query the database to find all State objects with a name
+    # containing the letter a
+    states_with_a = session.query(State).filter(State.name.like('%a%')).all()
 
-# Loop through the results and delete each object
-for state in states_with_a:
-    session.delete(state)
+    # Loop through the results and delete each object
+    for state in states_with_a:
+        session.delete(state)
 
-# Commit the changes to the database and close the session
-session.commit()
-session.close()
+    # Commit the changes to the database and close the session
+    session.commit()
+    session.close()
