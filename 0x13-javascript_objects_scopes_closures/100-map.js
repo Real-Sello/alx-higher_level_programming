acd1ba89ed4edeefce51d29ae84fc9becbd3fbbb
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-04-24 13:46:29 +0200

    Delete 0x13-javascript_objects_scopes_closures directory
    Remoded dorectory for deferral purposes

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const importedList = require('./100-data');
-
-const listValue = importedList.list;
-const newList = listValue.map((value, index) => value * index);
-
-console.log(listValue);
-console.log(newList);
