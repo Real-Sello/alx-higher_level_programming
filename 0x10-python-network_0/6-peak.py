ed69a456a4e2a0e205626a16ea833ebe897785b7
Real-Sello <spmoneatse@gmail.com>
2023-07-10 19:15:55 +0200

    Pycodestyle fix|Add blank line @ EOF && Remove long lines

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,8 +14,10 @@ def find_peak(list_of_integers):
         int: A peak element, or None if the list is empty.
 
     Complexity:
-        The complexity of this algorithm is O(log(n)), where n is the length of the list.
-        The algorithm utilizes a binary search approach to find a peak element efficiently.
+        The complexity of this algorithm is O(log(n)),
+        where n is the length of the list.
+        The algorithm utilizes a binary search approach
+        to find a peak element efficiently.
     """
 
     if not list_of_integers:  # Check if the list is empty
@@ -34,4 +36,3 @@ def find_peak(list_of_integers):
             end = mid
 
     return list_of_integers[start]
-
