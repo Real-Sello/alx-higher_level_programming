13dbb8b6b16d1144c1db19c351cb3f47f2f8511c
Real-Sello <spmoneatse@gmail.com>
2023-07-12 10:38:19 +0200

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,19 +9,19 @@ import sys
 import requests
 
 if __name__ == "__main__":
+    URL = "http://0.0.0.0:5000/search_user"
+    q = ""
+
     if len(sys.argv) > 1:
         q = sys.argv[1]
-    else:
-        URL = "http://0.0.0.0:5000/search_user"
-        q = ""
+    values = {"q": q}
 
-    values = {'q': q}
     request = requests.post(URL, data=values)
     try:
         response = request.json()
-        if response:
-            print("[{}] {}".format(response['id'], response['name']))
-        else:
+        if response == {}:
             print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except (TypeError, ValueError):
         print("Not a valid JSON")
