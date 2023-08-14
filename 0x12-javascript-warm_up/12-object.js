1c4a1e905df93be6e87f9fcc0abde51f12fa4be9
Real-Sello <spmoneatse@gmail.com>
2023-08-14 16:51:57 +0200

    12. Object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+this below script was updated to replace the value 12 with 89
+*/
+const MyObject = {
+  type: 'object',
+  value: 12
+};
+/*
+MY CODE BELOW
+*/
+console.log(MyObject);
+MyObject.value = 89;
+console.log(MyObject);
