ab65cdb0c59494628cd7f28206954b7c727490c2
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-08-14 13:38:52 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-/*
-an empty class Rectangle that defines a rectangle:
-
-    -Using the class notation for defining our class
-*/
-class Rectangle {}
-
-module.exports = Rectangle;
