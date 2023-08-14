c947543cb9c57a2063c5eaef66753a64f22e88c1
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-08-14 13:38:34 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/100-let_me_const.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-/*
-script that modifies the value of myVar
-*/
-myVar = 333;
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-/*
-script that prints two arguments passed to it,
-in the following format: “ is ”
-*/
-const [, , firstArg, secondArg] = process.argv;
-console.log(`${firstArg} is ${secondArg}`);
