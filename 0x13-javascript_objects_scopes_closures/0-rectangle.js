7ba0f789c352c7e201c77ef1b4c5650109882374
Real-Sello <spmoneatse@gmail.com>
2023-08-15 08:39:11 +0200

    0. Rectangle #0

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+an empty class Rectangle that defines a rectangle:
+
+    -Using the class notation for defining our class
+*/
+class Rectangle {}
+
+module.exports = Rectangle;
