014d1b617ce63df3109f27eaebdc6f24ff3f1df6
Real-Sello <spmoneatse@gmail.com>
2023-09-03 22:56:50 +0200

    9. My GitHub '

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Python script that takes user GitHub credentials
+(username and password) and uses the GitHub API to display user id
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    auth = requests.auth.HTTPBasicAuth(username, password)
+    response = requests.get(url, auth=auth)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data.get("id")
+        print(user_id)
+    else:
+        print(None)
