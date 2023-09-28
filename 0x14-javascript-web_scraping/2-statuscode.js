79b1bddcaefc1e4110552070014489c288a8780f
Real-Sello <spmoneatse@gmail.com>
2023-09-28 23:15:23 +0200

    2. Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const argu = process.argv;
+
+const url = argu[2];
+
+const request = require('request');
+
+request(url, function (error, response) {
+  console.log('code:', response && response.statusCode);
+  if (error) {
+    return console.log(error);
+  }
+});
