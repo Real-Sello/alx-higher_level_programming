5156f6edecc0cd1201efe762d7b61fc041195667
Real-Sello <spmoneatse@gmail.com>
2023-10-04 09:29:47 +0200

    13. And press ENTER

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('#btn_translate, #language_code').on('click keypress', function (event) {
+    if (event.type === 'click' || (event.type === 'keypress' && event.which === 13)) {
+      const languageCode = $('#language_code').val();
+      const apiUrl = `https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`;
+
+      $.getJSON(apiUrl, function (data) {
+        $('#hello').text(data.hello);
+      });
+    }
+  });
+});
